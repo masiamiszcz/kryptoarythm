5718b1ab902303746759fda5cb9c5a89a39c1717
masiamiszcz <147606068+masiamiszcz@users.noreply.github.com>
2024-11-17 11:29:59 +0100

    Finished

--- kryptoarythm/Program.cs
+++ kryptoarythm/Program.cs
@@ -6,21 +6,36 @@ namespace kryptoarythm
     {
         static void Main(string[] args)
         {
+            // Tablica przechowująca 7 unikalnych cyfr (A-G)
             int[] tab = new int[7];
+
+            // Zbiór do przechowywania wygenerowanych kombinacji
             HashSet<string> kombinacje = new HashSet<string>();
+
+            // Lista dostępnych cyfr
             List<int> availableints = new List<int>();
-            Random random = new Random();
             availableints = Enumerable.Range(0,10).ToList();
-            
+
+            //Generator liczb losowych
+            Random random = new Random();
+
+            // Główna pętla programu
             bool prawda = true;
             while (prawda)
             {
+
+                // Pętla generująca unikalną kombinację cyfr
                 bool jest = true;
                 while (jest)
                 {
                     if(kombinacje.Count == 604800) break;
+
+                    //Kopiowanie Listy dostępnych cyfr, aby nie usunąć oryginalnej listy
                     List<int> ints = new List<int>(availableints);
+
                     StringBuilder tablica = new StringBuilder();
+
+                    // Losowanie unikalnych cyfr i ich dodanie do tablicy `tab`
                     for (int i = 0; i < 7; i++)
                     {
                         int losindex = random.Next(ints.Count);
@@ -29,6 +44,7 @@ namespace kryptoarythm
                         tab[i] = los;
                         tablica.Append(los);
                     }
+                    //Sprawdzanie unikalności kombinacji
                     if (!kombinacje.Contains(tablica.ToString()))
                     {
                         kombinacje.Add(tablica.ToString()); jest = false;
@@ -38,34 +54,47 @@ namespace kryptoarythm
                         continue;
                     }
                 }
+
                 if (kombinacje.Count == 604800) 
                 {
                     Console.WriteLine("osiągnięto 604800 kombinacji, 100% możliwych kombinacji");
                     kombinacje.Clear();
                 } 
+
                 int A = tab[0], B = tab[1], C = tab[2], D = tab[3], E = tab[4], F = tab[5], G = tab[6];
 
                 int ABCDE = A * 10000 + B * 1000 + C * 100 + D * 10 + E;
-                int FGA = F * 100 + G * 10 + A;
+              
                 int ABCDE_A = ABCDE * A;
+                
+                //Sprawdzanie warunków zadania
                 if (ABCDE_A.ToString().Length == 6)
                 {
                     string H = ABCDE_A.ToString()[2].ToString();    
                     int ABCDE_G = ABCDE * G;
                     if (ABCDE_G.ToString().Length == 5)
                     {
+
                         if (ABCDE_G.ToString()[0] == B.ToString()[0] && ABCDE_G.ToString()[1] == B.ToString()[0]) 
                         { 
+
                             int ABCDE_F = ABCDE * F;
+
                             if(ABCDE_F.ToString().Length == 6) 
                             {
+
                                 if(ABCDE_F.ToString()[2] == H[0])
                                 {
+
                                     int wynik = ABCDE_A+ABCDE_F*100+ABCDE_G*10;
+
                                     if(wynik.ToString().Length == 8) 
                                     {
+
                                         if (wynik.ToString()[1] == F.ToString()[0] && wynik.ToString()[5] == D.ToString()[0]) 
                                         {
+
+                                            //Wypisanie wynik, komibinacji i przerwanie pętli
                                             Console.WriteLine($"A: {A}, B: {B}, C: {C}, D: {D}, E: {E}, F: {F}, G: {G} ");
                                             Console.WriteLine("wynik: " + wynik);
                                             prawda = false;
