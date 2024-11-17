64754680191d79db00a0319845abf43a39149850
masiamiszcz <147606068+masiamiszcz@users.noreply.github.com>
2024-11-17 21:24:26 +0100

    XD

--- kryptoarythm/Program.cs
+++ kryptoarythm/Program.cs
@@ -1,4 +1,10 @@
-﻿using System.Text;
+﻿
+//Maciej Kubacka
+//ISS 
+//15436
+
+
+using System.Text;
 
 namespace kryptoarythm
 {
@@ -7,7 +13,7 @@ namespace kryptoarythm
         static void Main(string[] args)
         {
             // Tablica przechowująca 7 unikalnych cyfr (A-G)
-            int[] tab = new int[7];
+            int[] tab = new int[8];
 
             // Zbiór do przechowywania wygenerowanych kombinacji
             HashSet<string> kombinacje = new HashSet<string>();
@@ -28,7 +34,7 @@ namespace kryptoarythm
                 bool jest = true;
                 while (jest)
                 {
-                    if(kombinacje.Count == 604800) break;
+                    if(kombinacje.Count == 3628000) break;
 
                     //Kopiowanie Listy dostępnych cyfr, aby nie usunąć oryginalnej listy
                     List<int> ints = new List<int>(availableints);
@@ -36,7 +42,7 @@ namespace kryptoarythm
                     StringBuilder tablica = new StringBuilder();
 
                     // Losowanie unikalnych cyfr i ich dodanie do tablicy `tab`
-                    for (int i = 0; i < 7; i++)
+                    for (int i = 0; i < 8; i++)
                     {
                         int losindex = random.Next(ints.Count);
                         int los = ints[losindex];
@@ -55,91 +61,98 @@ namespace kryptoarythm
                     }
                 }
 
-                if (kombinacje.Count == 604800) 
+                if (kombinacje.Count == 3628000) 
                 {
-                    Console.WriteLine("osiągnięto 604800 kombinacji, 100% możliwych kombinacji");
+                    Console.WriteLine("osiągnięto 3628000 kombinacji, 100% możliwych kombinacji");
                     kombinacje.Clear();
                 } 
 
-                int A = tab[0], B = tab[1], C = tab[2], D = tab[3], E = tab[4], F = tab[5], G = tab[6];
+                int A = tab[0], B = tab[1], C = tab[2], D = tab[3], E = tab[4], F = tab[5], G = tab[6], H = tab[7];
 
                 int ABCDE = A * 10000 + B * 1000 + C * 100 + D * 10 + E;
               
                 int ABCDE_A = ABCDE * A;
-                
+
                 //Sprawdzanie warunków zadania
                 if (ABCDE_A.ToString().Length == 6)
                 {
-                    string H = ABCDE_A.ToString()[2].ToString();    
-                    int ABCDE_G = ABCDE * G;
-                    if (ABCDE_G.ToString().Length == 5)
+                    if (ABCDE_A.ToString()[2] == H.ToString()[0]) 
                     {
+                        int ABCDE_G = ABCDE * G;
+                        if (ABCDE_G.ToString().Length == 5)
+                        {
 
-                        if (ABCDE_G.ToString()[0] == B.ToString()[0] && ABCDE_G.ToString()[1] == B.ToString()[0]) 
-                        { 
-
-                            int ABCDE_F = ABCDE * F;
-
-                            if(ABCDE_F.ToString().Length == 6) 
+                            if (ABCDE_G.ToString()[0] == B.ToString()[0] && ABCDE_G.ToString()[1] == B.ToString()[0])
                             {
 
-                                if(ABCDE_F.ToString()[2] == H[0])
+                                int ABCDE_F = ABCDE * F;
+
+                                if (ABCDE_F.ToString().Length == 6)
                                 {
 
-                                    int wynik = ABCDE_A+ABCDE_F*100+ABCDE_G*10;
-
-                                    if(wynik.ToString().Length == 8) 
+                                    if (ABCDE_F.ToString()[2] == H.ToString()[0])
                                     {
 
-                                        if (wynik.ToString()[1] == F.ToString()[0] && wynik.ToString()[5] == D.ToString()[0]) 
+                                        int wynik = ABCDE_A + ABCDE_F * 100 + ABCDE_G * 10;
+
+                                        if (wynik.ToString().Length == 8)
                                         {
 
-                                            //Wypisanie wynik, komibinacji i przerwanie pętli
-                                            Console.WriteLine($"A: {A}, B: {B}, C: {C}, D: {D}, E: {E}, F: {F}, G: {G} ");
-                                            Console.WriteLine("wynik: " + wynik);
-                                            prawda = false;
+                                            if (wynik.ToString()[1] == F.ToString()[0] && wynik.ToString()[5] == D.ToString()[0])
+                                            {
+
+                                                //Wypisanie wynik, komibinacji i przerwanie pętli
+                                                Console.WriteLine($"A: {A}, B: {B}, C: {C}, D: {D}, E: {E}, F: {F}, G: {G} H: {H}");
+                                                Console.WriteLine("wynik: " + wynik);
+                                                prawda = false;
+                                            }
+                                            else
+                                            {
+                                                //Console.WriteLine("7");
+                                                continue;
+                                            }
                                         }
                                         else
                                         {
-                                            //Console.WriteLine("7");
+                                            //Console.WriteLine("6");
                                             continue;
                                         }
                                     }
-                                    else 
+                                    else
                                     {
-                                        //Console.WriteLine("6");
+                                        //Console.WriteLine("5");
                                         continue;
                                     }
                                 }
                                 else
                                 {
-                                    //Console.WriteLine("5");
+                                    //Console.WriteLine("4");
                                     continue;
-                                }    
+                                }
                             }
                             else
                             {
-                                //Console.WriteLine("4");
+
+                                //Console.WriteLine("3");
                                 continue;
                             }
                         }
                         else
                         {
-                            
-                            //Console.WriteLine("3");
+                            //Console.WriteLine("2");
                             continue;
                         }
                     }
-                    else 
+                    else
                     {
-                        //Console.WriteLine("2");
+                        Console.WriteLine("1.2");
                         continue;
                     }
                 }
-                else 
+                else
                 {
                     //Console.WriteLine("1");
-                    continue; 
+                    continue;
                 }
 
             }
